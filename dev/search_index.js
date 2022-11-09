2fb88c574de178dad58045130fcdb9edaa2bde5d
Documenter.jl <documenter@juliadocs.github.io>
2022-11-09 17:24:32 +0000

    build based on 8a1b0e6

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = xlab","category":"page"},{"location":"#xlab","page":"Home","title":"xlab","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for xlab.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [xlab]","category":"page"}]
+}
